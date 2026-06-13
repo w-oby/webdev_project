70aa5b662e74367d52dcc30a062e3d361d61fbe0
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2026-06-13 09:55:57 +0200

    Fixed TS config

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+const toggleButton = document.getElementById("themeToggle");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    const icon = toggleButton.querySelector("i");
+    if (document.body.classList.contains("dark-mode")) {
+        icon === null || icon === void 0 ? void 0 : icon.classList.remove("fa-moon");
+        icon === null || icon === void 0 ? void 0 : icon.classList.add("fa-sun");
+    }
+    else {
+        icon === null || icon === void 0 ? void 0 : icon.classList.remove("fa-sun");
+        icon === null || icon === void 0 ? void 0 : icon.classList.add("fa-moon");
+    }
+});
