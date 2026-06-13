4cfedceb50506ad55664d99e2f6e987055b2ee79
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2026-06-13 09:50:23 +0200

    Added TS code and dark mode styling

--- ts/script.js
+++ ts/script.js
@@ -1,21 +1,16 @@
-const toggleButton = document.getElementById("themeToggle") as HTMLButtonElement;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const toggleButton = document.getElementById("themeToggle");
 toggleButton.addEventListener("click", () => {
-
     document.body.classList.toggle("dark-mode");
-
     const icon = toggleButton.querySelector("i");
-
     if (document.body.classList.contains("dark-mode")) {
-
         icon?.classList.remove("fa-moon");
         icon?.classList.add("fa-sun");
-
-    } else {
-
+    }
+    else {
         icon?.classList.remove("fa-sun");
         icon?.classList.add("fa-moon");
-
     }
-
 });
+//# sourceMappingURL=script.js.map
